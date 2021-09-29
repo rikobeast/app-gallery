a11cffbf1f5b69938387196432350f1ccf0b941d
Zhivko Minchev <jivkominchev.j@gmail.com>
2021-09-29 17:30:03 +0300

    First Commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <div className="navigation">
+            <ul>
+                <li>ASD</li>
+                <li>SAD</li>
+                <li>SFA</li>
+                <li>AWS</li>
+            </ul>
+        </div>
+    )
+}
+export default NavBar
